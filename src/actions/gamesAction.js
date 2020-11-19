7a2ff3faf0c467a9abd58fd1226537dfb6e5a74c
Geoffrey <geoffrey.couten@gmail.com>
2020-11-19 13:50:00 +0100

    fetching data

---
+++ src/actions/gamesAction.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from '../api'
+
+export const loadGames = () => async (dispatch) => {
+  // fetch axios
+  const popularData = await axios.get(popularGamesURL())
+  const upcomingData = await axios.get(upcomingGamesURL())
+  const newGamesData = await axios.get(newGamesURL())
+  dispatch({
+    type: 'FETCH_GAMES',
+    payload: {
+      popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesData.data.results,
+    },
+  })
+}
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -8,7 +8,12 @@ const initialState = {
 const gamesReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_GAMES':
-      return { ...state }
+      return {
+        ...state,
+        popular: action.payload.popular,
+        upcoming: action.payload.upcoming,
+        newGames: action.payload.newGames,
+      }
     default:
       return { ...state }
   }
